b61633dcf872943b962962d190205150947ff980
Caillin Nugent <caillinn@student.unimelb.edu.au>
2026-04-30 06:48:59 +1000

    Refined experiment header - made classes templated - added explainer comment at top - added include guards

--- include/experiment/experiment.hpp
+++ include/experiment/experiment.hpp
@@ -1,40 +1,54 @@
+/*
+ * Declaration of Experiment class, and associated pure
+ * virtual classes
+ * /
+
+#ifndef EXPERIMENT_H
+#define EXPERIMENT_H
+
 #include <vector>
 
 
-// should be templated later
-class State {
-	std::vector<int> tiles;
-};
 
-
+template
+<typename State>
 struct Game {
-	std::vector<State> states;	
-	std::vector<State> get_legal_moves();
+	std::vector<State> get_legal_moves() = 0;
 	static bool is_winning(State& state, int player) = 0;
 };
 
+template
+
+<typename State>
 struct TrainingExample {
 	State state;
 	double v_train;
 };
 
+template
+<typename State>
 class Hypothesis {
 	// evaluate a game state using current approximation
 	virtual double get_value(State& state) = 0;
 };
 
 
+template
+<typename Game>
 class Generator {
 	// generate a starting state using hypothesis
 	Game get_initial(Hypothesis& hypothesis) = 0;
 };
 
-
+template
+<typename Game>
 class Performer {
 	// play a complete game from an initial state
 	void play_game(Hypothesis& hypothesis, Game& initial_state);
 };
 
+template
+<typename Game>
 class Critic {
 	// estimate vtrain using current hypothesis's evaluation of next state where it is player's turn
 	std::vector<TrainingExample> get_training_examples(Game& finished_game, Hypothesis& hypothesis) = 0;	
@@ -58,3 +72,5 @@ public:
 	// perform one loop of generate -> perform -> critique -> update
 	void perform();
 };
+
+#endif // EXPERIMENT_H
