02a5be4ba27d1dec6efdb4ac3868bdd4357badf7
Caillin Nugent <caillinn@student.unimelb.edu.au>
2026-04-30 14:16:28 +1000

    made experiment interface methods virtual, added virtual destructors

--- include/experiment/experiment.hpp
+++ include/experiment/experiment.hpp
@@ -1,24 +1,27 @@
 /*
  * Declaration of Experiment class, and associated pure
  * virtual classes
- * /
+ * */
 
 #ifndef EXPERIMENT_H
 #define EXPERIMENT_H
 
 #include <vector>
 
-
+namespace experiment
+{
 
 template
 <typename State>
 struct Game {
-	std::vector<State> get_legal_moves() = 0;
-	static bool is_winning(State& state, int player) = 0;
+public:
+	virtual std::vector<State> get_legal_moves() = 0;
+	virtual bool play_move(State next_legal_move) = 0;
+	virtual bool is_winning(State& state, int player) = 0;
+	virtual ~Game() {}
 };
 
 template
-
 <typename State>
 struct TrainingExample {
 	State state;
@@ -30,47 +33,57 @@ template
 class Hypothesis {
 	// evaluate a game state using current approximation
 	virtual double get_value(State& state) = 0;
+	virtual ~Hypothesis() {}
 };
 
 
 template
-<typename Game>
+<typename State>
 class Generator {
+public:
 	// generate a starting state using hypothesis
-	Game get_initial(Hypothesis& hypothesis) = 0;
+	virtual Game<State> get_initial(Hypothesis<State>& hypothesis) = 0;
+	virtual ~Generator() {}
 };
 
 template
-<typename Game>
+<typename State>
 class Performer {
 	// play a complete game from an initial state
-	void play_game(Hypothesis& hypothesis, Game& initial_state);
+	virtual void play_game(Hypothesis<State>& hypothesis, Game<State>& initial_state);
+	virtual ~Performer() {}
 };
 
 template
-<typename Game>
+<typename State>
 class Critic {
 	// estimate vtrain using current hypothesis's evaluation of next state where it is player's turn
-	std::vector<TrainingExample> get_training_examples(Game& finished_game, Hypothesis& hypothesis) = 0;	
+	virtual std::vector<TrainingExample<State>> get_training_examples(Game<State>& finished_game, Hypothesis<State>& hypothesis) = 0;	
+	virtual ~Critic() {}
 };
 
+template
+<typename State>
 class Generalizer {
 	// get new hypothesis using training examples
-	Hypothesis update_approximation(std::vector<TrainingExample>& training_examples, Hypothesis& curr_hypothesis) = 0;
+	virtual Hypothesis<State> update_approximation(std::vector<TrainingExample<State>>& training_examples, Hypothesis<State>& curr_hypothesis) = 0;
+	virtual ~Generalizer() {}
 };
 
-
+template
+<typename State>
 class Experiment {
 private:
-	Hypothesis hypothesis_;
-	Generator generator_;
-	Performer performer_;
-	Critic critic_;
-	Generalizer generalizer_;
+	Hypothesis<State> hypothesis_;
+	Generator<State> generator_;
+	Performer<State> performer_;
+	Critic<State> critic_;
+	Generalizer<State> generalizer_;
 public:
-	Experiment()
+	Experiment();
 	// perform one loop of generate -> perform -> critique -> update
 	void perform();
 };
 
+} // namespace experiment
 #endif // EXPERIMENT_H
